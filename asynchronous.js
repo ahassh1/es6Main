8debe24889e85abbb3936a3a2482b43aef8d48d9
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-09 10:13:10 +0600

    asynnchronous task done

--- asynchronous.js
+++ asynchronous.js
@@ -2,7 +2,25 @@ const a= 20;
 const b = 30;
 
 console.log('first item ')
-console.log('second item ')
+
+// async start
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+.then(data => console.log(data))
+
+setTimeout(() =>{
+ console.log( 'second items')
+},3000)
+
+setTimeout(task)
+// async section end 
+
+task()
+console.log('third item ')
 
 const result= a+b;
 console.log(result)
+
+function task(){
+    console.log('last test')
+}
